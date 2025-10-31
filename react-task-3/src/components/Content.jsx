f589f73ef5f9e6e7703ce6fa75bde9cb63d40dbf
viblo29 <dachivibliani29@gmail.com>
2025-11-01 00:38:41 +0400

    Added featureCards component

--- react-task-3/src/components/Content.jsx
+++ react-task-3/src/components/Content.jsx
@@ -2,6 +2,7 @@ import Header from "./Header";
 import "./Content.css";
 import Arrow from "../../public/white-arrow.svg";
 import Infos from "./Infos";
+import FeatureCards from "./FeatureCards";
 
 
 function Content() {
@@ -31,6 +32,21 @@ function Content() {
             description="Using a collaborative formula of designers, researchers, photographers, videographers, and copywriters, we’ll build and extend your brand in digital places. "
         />
         <div className="info-image2"></div>
+      </div>
+
+      <div className="feature-cards">
+        <FeatureCards
+          title="Graphic Design"
+          color="#24554A"
+          backgroundImage= "../../public/cherry.svg"
+          description="Great design makes you memorable. We deliver artwork that underscores your brand message and captures potential clients’ attention."
+        />
+        <FeatureCards 
+          title="Photography"
+          color="#052C3B"
+          backgroundImage="../../public/mandarin.svg"
+          description="Increase your credibility by getting the most stunning, high-quality photos that improve your business image."
+        />
       </div>
     </>
   );
